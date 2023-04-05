0d56c5bac29c966266716c7dedf0e40c3dbfbf60
EricH-learning <92173968+EricH-learning@users.noreply.github.com>
2023-04-05 21:51:55 +0800

    add categories style scss, improve categories by using map

--- src/App.js
+++ src/App.js
@@ -1,9 +1,44 @@
+import "./categories.styles.scss";
+
 const App = () => {
+  const categories = [
+    {
+      id: "01",
+      title: "Hats",
+    },
+    {
+      id: "02",
+      title: "Jackets",
+    },
+    {
+      id: "03",
+      title: "Sneakers",
+    },
+    {
+      id: "04",
+      title: "Womens",
+    },
+    {
+      id: "05",
+      title: "Mens",
+    },
+  ];
+
   return (
-    <div>
-      <p>Hello</p>
+    <div className="categories-container">
+      {categories.map((category) => {
+        return (
+          <div className="category-container" key={category.id}>
+            <div className="background-image"></div>
+            <div className="category-body-container">
+              <h2>{category.title}</h2>
+              <p>Shop Now</p>
+            </div>
+          </div>
+        );
+      })}
     </div>
-  )``;
+  );
 };
 
 export default App;
